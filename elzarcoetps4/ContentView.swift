6eaf02b1c3418b4b8486f37f9f397153032a3e42
Luis2506452019 <2506452019@mail.utec.edu.sv>
2023-11-19 19:43:50 -0800

    Initial Commit

---
+++ elzarcoetps4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  elzarcoetps4
+//
+//  Created by MacOsX on 11/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ elzarcoetps4/elzarcoetps4App.swift
@@ -0,0 +1,17 @@
+//
+//  elzarcoetps4App.swift
+//  elzarcoetps4
+//
+//  Created by MacOsX on 11/19/23.
+//
+
+import SwiftUI
+
+@main
+struct elzarcoetps4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
